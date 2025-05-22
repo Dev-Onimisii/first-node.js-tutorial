1dc86d736427b6ff6795a97c5ff8ef5105a8144e
Isyak Abdulrasheed <abdulrasheedisyak44@gmailcom>
2025-05-22 13:47:45 +0100

    Commit

--- express.js
+++ express.js
@@ -2,20 +2,20 @@
 // express.js
 
 const express = require("express"); // 1
-const app = express(); // 2
+const mainExpress = express(); // 2
 const port = 5000; // 3
 
 // 4
-app.get("/", (req, res) => {
+mainExpress.get("/", (req, res) => {
   res.send("<h1>Welcome to (express.js) homeage</h1>"); // 5
 });
 
 // 6
-app.get("/login", (req, res) => {
+mainExpress.get("/login", (req, res) => {
   res.send("<h1> This is the login page of (express.js)");
 });
 
 // 7
-app.listen(port, () => {
+mainExpress.listen(port, () => {
   console.log(`Express server listening at http://localhost:${port}`);
 });
