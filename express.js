5705d75b1a7bf4de73e66a195638407f5b6909cc
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2025-05-23 01:28:46 +0100

    Made changes

--- express.js
+++ express.js
@@ -1,21 +1,18 @@
 // My Assignment
 // express.js
 
-const express = require("express"); // 1
-const mainExpress = express(); // 2
-const port = 5000; // 3
+const express = require("express");
+const mainExpress = express();
+const port = 5000;
 
-// 4
 mainExpress.get("/", (req, res) => {
-  res.send("<h1>Welcome to (express.js) homeage</h1>"); // 5
+  res.send("<h1>Welcome to (express.js) homeage</h1>");
 });
 
-// 6
 mainExpress.get("/login", (req, res) => {
   res.send("<h1> This is the login page of (express.js)");
 });
 
-// 7
 mainExpress.listen(port, () => {
   console.log(`Express server listening at http://localhost:${port}`);
 });
